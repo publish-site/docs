6f3410c2c8ce813dad596d14cc05116d7c3523ae
a-rvid <a-rvid@rvid.eu>
2026-02-10 17:28:01 +0100

    PKI

--- pki.sh
+++ pki.sh
@@ -33,17 +33,12 @@ echo -e "\033[92mGenerating client certificates (mTLS)\033[0m"
 openssl req -newkey rsa:4096 -nodes -keyout client.key -out client.csr -subj "/CN=actions" # Client key and csr
 openssl x509 -req -in client.csr -CA CA.pem -CAkey /dev/shm/mtls/CA.key -out client.pem -sha256 # Client certificate
 
-echo -e "\033[32mServer Configuration\n\033[92mCLIENT_CA\033[0m:"
-base64 "CA.pem" -w 0
-printf "\n\n\n"
-
-echo -e "\033[32mWorkflow Configuration\n\033[92mprivkey \033[0m(add as action SECRET!):"
-base64 client.key -w 0
 printf "\n\n"
 echo -e "\033[92mcert \033[0m(add as action SECRET):"
 base64 client.pem -w 0
 printf "\n\n\n"
 
-cd "$PWD"
+#cd "$PWD"
 rm -rf /dev/shm/mtls 
-echo -e "\033[32mCertificates can be directly mounted and are located at \033[0m$TMP"
+echo -e "\033[32mcertificates can be directly mounted. The certificates are located at \033[0m$TMP"
+echo -e "\033[32mPRIVKEY (actions secret) should be\033[0m client.key\n\033[32mCERT (actions secret) should be \033[0m client.pem"
